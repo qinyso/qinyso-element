b9e26b3674d080a722c58805288f259b03b88585
qinyso <1498095474@qq.com>
2025-06-04 11:16:27 +0800

    build

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,6 @@
 import  {makeInstaller}  from '../utils/install';
 import components from './components'; 
+import '@toy-element/theme/index.css'
 const installer = makeInstaller(components);
 export * from '../components';
 export default installer
